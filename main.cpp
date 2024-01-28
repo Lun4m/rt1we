eb0eb1348a816e4944275c037474325d99f8501a
Manuel Carrer <manuel.carrer@kjemi.uio.no>
2024-01-28 16:51:31 +0100

    Use surface normals

--- main.cpp
+++ main.cpp
@@ -2,20 +2,28 @@
 #include "ray.h"
 #include "vec3.h"
 
+#include <cmath>
 #include <iostream>
 
-bool hit_sphere(const point3 &center, double radius, const Ray &r) {
+double hit_sphere(const point3 &center, double radius, const Ray &r) {
   vec3 oc = r.origin() - center;
-  auto a = dot(r.direction(), r.direction());
-  auto b = 2.0 * dot(oc, r.direction());
-  auto c = dot(oc, oc) - radius * radius;
-  auto discriminant = b * b - 4 * a * c;
-  return discriminant >= 0;
+  auto a = r.direction().length_squared();
+  auto half_b = dot(oc, r.direction());
+  auto c = oc.length_squared() - radius * radius;
+  auto discriminant = half_b * half_b - a * c;
+
+  if (discriminant < 0) {
+    return -1.0;
+  } else {
+    return -half_b - sqrt(discriminant) / a;
+  }
 }
 
 color ray_color(const Ray &r) {
-  if (hit_sphere(point3(0, 0, 1), 0.5, r)) {
-    return color(1, 0, 0);
+  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+  if (t > 0.0) {
+    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
   }
 
   vec3 unit_direction = unit_vector(r.direction());
@@ -46,7 +54,7 @@ int main(int argc, char *argv[]) {
   auto pixel_delta_u = viewport_u / image_width;
   auto pixel_delta_v = viewport_v / image_height;
 
-  auto viewport_upper_left = camera_center + vec3(0, 0, focal_length) -
+  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
                              viewport_u / 2 - viewport_v / 2;
 
   auto pixel00_loc =
