437af742a4c89d76a09d0fc3741d1d1ff56856a3
Manuel Carrer <manuel.carrer@kjemi.uio.no>
2024-01-29 11:19:36 +0100

    Add multiple sample antialiasing

--- camera.h
+++ camera.h
@@ -11,6 +11,7 @@ class Camera {
 public:
   double aspect_ratio = 1.0;
   int image_width = 100;
+  int samples_per_pixel = 10;
 
   void render(const Hittable &world) {
     initialize();
@@ -22,13 +23,13 @@ public:
       std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
                 << std::flush;
       for (int i = 0; i < image_width; i++) {
-        auto pixel_center =
-            pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-        auto ray_direction = pixel_center - center;
-        Ray r(center, ray_direction);
+        Color pixel_color(0, 0, 0);
+        for (int sample = 0; sample < samples_per_pixel; sample++) {
+          Ray r = get_ray(i, j);
+          pixel_color += ray_color(r, world);
+        }
 
-        Color pixel_color = ray_color(r, world);
-        write_color(std::cout, pixel_color);
+        write_color(std::cout, pixel_color, samples_per_pixel);
       }
     }
 
@@ -66,6 +67,23 @@ private:
     auto viewport_upper_left =
         center - Vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+  }
+
+  Ray get_ray(int i, int j) const {
+    // Random sample camera ray for pixel (i,j)
+    auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+    auto pixel_sample = pixel_center + pixel_sample_square();
+
+    auto ray_origin = center;
+    auto ray_direction = pixel_sample - ray_origin;
+    return Ray(ray_origin, ray_direction);
+  }
+
+  Vec3 pixel_sample_square() const {
+    // Random point surrounding a pixel (at the origin)
+    auto px = -0.5 + random_double();
+    auto py = -0.5 + random_double();
+    return (px * pixel_delta_u) + (py * pixel_delta_v);
   }
 
   Color ray_color(const Ray &r, const Hittable &world) const {
